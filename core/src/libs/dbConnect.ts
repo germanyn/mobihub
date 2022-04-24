29dd706705311d3a7296191f41e0362f81adec92
Germano Gascho <germanogascho@gmail.com>
2022-04-24 20:40:58 -0300

    reverte

--- core/src/libs/dbConnect.ts
+++ core/src/libs/dbConnect.ts
@@ -6,8 +6,7 @@ https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/uti
 **/
 
 
-// const MONGODB_URI = process.env.MONGODB_URI!
-const MONGODB_URI = ''
+const MONGODB_URI = process.env.MONGODB_URI!
 console.log('MONGODB_URI', MONGODB_URI)
 
 if (!MONGODB_URI) {
