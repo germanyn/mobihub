62a187a90d04f9922d3f3061768a7ecb247c0dac
Germano Gascho <germanogascho@gmail.com>
2022-04-24 14:13:09 -0300

    ajusta compilação de typescript monorepo

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,34 @@
+const path = require('path')
+
+// next.config.js
+const aliasPathsToResolve = [
+  { name: "@mobihub/core", path: path.resolve(__dirname, "../core") },
+];
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+module.exports = () => {
+  return {
+    reactStrictMode: true,
+    webpack(config, { defaultLoaders }) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: [path.resolve(__dirname, "../core")],
+        use: [defaultLoaders.babel],
+      });
+
+      /** Resolve aliases */
+      aliasPathsToResolve.forEach((module) => {
+        config.resolve.alias[module.name] = module.path;
+      });
+      return config
+    },
+    plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+  };
 };
+
+// // next.config.js
+// const withTM = require('next-transpile-modules')(['@mobihub/core']); // pass the modules you would like to see transpiled
+
+// module.exports = withTM({
+//   reactStrictMode: true,
+// });
